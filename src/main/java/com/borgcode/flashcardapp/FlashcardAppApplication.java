db407e8cfed098d6babd47010ff65d2c3391ecce
KieranB <kierans.borg@gmail.com>
2023-07-20 22:51:13 +0200

    Removed CommandLineRunner implementation

--- src/main/java/com/borgcode/flashcardapp/FlashcardAppApplication.java
+++ src/main/java/com/borgcode/flashcardapp/FlashcardAppApplication.java
@@ -12,33 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class FlashcardAppApplication implements CommandLineRunner {
+public class FlashcardAppApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(FlashcardAppApplication.class, args);
 
     }
-    @Autowired
-    private CardRepository cardRepository;
-    @Autowired
-    private DeckRepository deckRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        /*Card card1 = new Card("My name", "Your Name");
-        Card card2 = new Card("ipsum", "lorem");
-        Card card3 = new Card("hello", "yes");
-        cardRepository.save(card1);
-        cardRepository.save(card2);
-        cardRepository.save(card3);
-
-        Deck deck = new Deck("Test", List.of(card1, card2, card3));
-        deckRepository.save(deck);
-
-        deckRepository.save(new Deck("Test2"));*/
-
-
-
-    }
 }
